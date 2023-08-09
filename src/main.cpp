16e8822eeaafe29390b0eef4acc984f1b7279da4
krisstakos <kristianchok@gmail.com>
2023-08-10 00:24:02 +0300

    proper implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,24 @@
 #include <Arduino.h>
+#include <Bounce2.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
+#include "mbedtls/aes.h"
 
-#define SERVICE_UUID_FFF "0000fff0-0000-1000-8000-00805f9b34fb"
-#define CHARACTERISTIC_UUID_FFF1 "d44bc439-abfd-45a2-b575-925416129600" /*- Write, Write no response */ //directions
+#define OLD_COLLECTION_PREFIX "QCAR"
+#define CONTROL_SERVICE_UUID "0000fff0-0000-1000-8000-00805f9b34fb"
+#define CONTROL_CHARACTERISTIC_UUID "d44bc439-abfd-45a2-b575-925416129600" /*- Write, Write no response */  //directions
+#define BATTERYCHARACTERISTIC_UUID "d44bc439-abfd-45a2-b575-925416129601" /*- Write, Write no response */   //battery
 
 #define SERVICE_UUID_1800 "00001800-0000-1000-8000-00805f9b34fb"
-#define CHARACTERISTIC_UUID_1800 "00002a00-0000-1000-8000-00805f9b34fb" /* - Write no response */ //name
+#define CHARACTERISTIC_UUID_1800 "00002a00-0000-1000-8000-00805f9b34fb" /* - Write no response */  //name
 
-uint8_t idle_data[16] = { 0x02, 0x5e, 0x69, 0x5a, 0x48, 0xff, 0x2a, 0x43, 0x8c, 0xa6, 0x80, 0xf8, 0x3e, 0x04, 0xe4, 0x5d };
-uint8_t up_data[16] = { 0x29, 0x60, 0x9c, 0x66, 0x48, 0x52, 0xcf, 0xf1, 0xb0, 0xf0, 0xcb, 0xb9, 0x80, 0x14, 0xbd, 0x2c };
-uint8_t down_data[16] = { 0x03, 0x20, 0x99, 0x09, 0xba, 0x9d, 0xa1, 0xc8, 0xb9, 0x86, 0x16, 0x3c, 0x6d, 0x48, 0x46, 0x55 };
-uint8_t left_data[16] = { 0x51, 0x38, 0x21, 0x12, 0x13, 0x5c, 0xcc, 0xdb, 0x46, 0xcf, 0x89, 0x21, 0xb7, 0x05, 0x49, 0x9a };
-uint8_t right_data[16] = { 0x1b, 0x57, 0x69, 0xcd, 0xf1, 0x3e, 0x8a, 0xb6, 0x27, 0x08, 0x0f, 0xf3, 0xce, 0xfc, 0x3b, 0xc0 };
-
-uint8_t up_left_data[16] = { 0x99, 0x28, 0xe5, 0x90, 0xdf, 0xe8, 0x21, 0x48, 0x5f, 0x41, 0x4f, 0xbb, 0x63, 0x3d, 0x5c, 0x4e };
-uint8_t up_right_data[16] = { 0x0f, 0x2c, 0xe5, 0x66, 0x62, 0xd4, 0xfd, 0x9d, 0x32, 0xa4, 0x4f, 0x10, 0x2b, 0xf2, 0x0a, 0xa7 };
-uint8_t down_left_data[16] = { 0x98, 0xce, 0x98, 0x1d, 0x58, 0xd1, 0x15, 0xaf, 0xe1, 0x19, 0x60, 0xbf, 0x46, 0x13, 0x92, 0x5c };
-uint8_t down_right_data[16] = { 0xf2, 0x52, 0x0f, 0xba, 0x31, 0x44, 0xfb, 0x11, 0x46, 0x8f, 0xe0, 0x80, 0xc6, 0xc2, 0xc2, 0x3c };
-
-
-static BLEUUID serviceUUID(SERVICE_UUID_FFF);        //Service UUID
-static BLEUUID charUUID(CHARACTERISTIC_UUID_FFF1);   //Characteristic  UUID
-String connectto_car_address = "00:3c:a0:03:fc:5d";  //Hardware Bluetooth MAC of my shell racing, use "nRF Connect" to inspect
+static BLEUUID controlUUID(CONTROL_SERVICE_UUID);             //Service UUID
+static BLEUUID controCharlUUID(CONTROL_CHARACTERISTIC_UUID);  //Characteristic  UUID
+static BLEUUID batteryUUID(BATTERYCHARACTERISTIC_UUID);       //Characteristic  UUID
+String qcar_car_address_prefix = "00:3c";                     //Hardware Bluetooth MAC of my shell racing, use "nRF Connect" to inspect
 
 static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLERemoteCharacteristic* batteryCharacteristic;
 
 BLEScan* pBLEScan;  //Name the scanning device as pBLEScan
 BLEScanResults foundDevices;
@@ -32,12 +26,23 @@ BLEScanResults foundDevices;
 static BLEAddress* Server_BLE_Address;
 String scanned_car_address;
 
-boolean paired = false;
+Bounce2::Button button = Bounce2::Button();
 
-#define PIN_UP 14
-#define PIN_DOWN 33
-#define PIN_LEFT 27
+mbedtls_aes_context aes;
+const uint8_t aes_key[16] = { 0x34, 0x52, 0x2A, 0x5B, 0x7A, 0x6E, 0x49, 0x2C, 0x08, 0x09, 0x0A, 0x9D, 0x8D, 0x2A, 0x23, 0xF8 };
+uint8_t plain_data[16] = { 0x00, 0x43, 0x54, 0x4c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t output_data[16];
+uint8_t decrypted_data[16];
+
+bool turbo_flag = 0;
+bool paired = false;
+
+#define PIN_FORWARD 15
+#define PIN_BACKWARDS 13
+#define PIN_LEFT 33
 #define PIN_RIGHT 32
+#define PIN_LIGHTS 35
+#define PIN_TURBO 16
 
 class MyClientCallback : public BLEClientCallbacks {
   void onConnect(BLEClient* pClient) {
@@ -51,6 +56,39 @@ class MyClientCallback : public BLEClientCallbacks {
   }
 };
 
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    scanned_car_address = advertisedDevice.getAddress().toString().c_str();
+    Serial.println(scanned_car_address);
+    if (scanned_car_address.indexOf(qcar_car_address_prefix) != std::string::npos) {
+      Server_BLE_Address = new BLEAddress(advertisedDevice.getAddress());
+    }
+  }
+};
+
+void craftBLErequest() {
+  plain_data[4] = !digitalRead(PIN_FORWARD);
+  plain_data[5] = !digitalRead(PIN_BACKWARDS);
+  plain_data[6] = !digitalRead(PIN_LEFT);
+  plain_data[7] = !digitalRead(PIN_RIGHT);
+  plain_data[8] = digitalRead(PIN_LIGHTS);
+  if (button.pressed()) {
+    if (turbo_flag) {
+      turbo_flag = 0;
+    } else {
+      turbo_flag = 1;
+    }
+    Serial.print("Turbo is ");
+    Serial.println(turbo_flag);
+  }
+  if (turbo_flag) {
+    plain_data[9] = 0x64;
+  } else {
+    plain_data[9] = 0x50;
+  }
+  mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_ENCRYPT, plain_data, output_data);
+}
+
 bool connectToserver(BLEAddress pAddress) {
   BLEClient* pClient = BLEDevice::createClient();
   Serial.println(" - Created client");
@@ -60,12 +98,12 @@ bool connectToserver(BLEAddress pAddress) {
   Serial.println(" - Connected to Shell Race Car");
 
   // Obtain a reference to the service we are after in the remote BLE server.
-  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  BLERemoteService* pRemoteService = pClient->getService(controlUUID);
   if (pRemoteService != nullptr) {
     Serial.println(" - Found our service");
-  } else
+  } else {
     return false;
-
+  }
   std::map<std::string, BLERemoteCharacteristic*>* cm = pRemoteService->getCharacteristics();
   std::map<std::string, BLERemoteCharacteristic*>::iterator it;
   for (it = cm->begin(); it != cm->end(); it++) {
@@ -75,61 +113,64 @@ bool connectToserver(BLEAddress pAddress) {
     Serial.println("");
   }
   // Obtain a reference to the characteristic in the service of the remote BLE server.
-  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(controCharlUUID);
+  batteryCharacteristic = pRemoteService->getCharacteristic(batteryUUID);
+  batteryCharacteristic->registerForNotify(batteryNotifyCallback);
   if (pRemoteCharacteristic != nullptr)
     Serial.println(" - Found our characteristic");
 
   return true;
 }
-
-class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
-  void onResult(BLEAdvertisedDevice advertisedDevice) {
-    scanned_car_address = advertisedDevice.getAddress().toString().c_str();
-    Serial.println(scanned_car_address);
-    if (scanned_car_address.equals(connectto_car_address)) {
-      Server_BLE_Address = new BLEAddress(advertisedDevice.getAddress());
-    }
-  }
-};
+//When the BLE Server sends a new battery reading with the notify property
+static void batteryNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic,
+                                  uint8_t* pData, size_t length, bool isNotify) {
+  //store battery value
+  mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_DECRYPT, pData, decrypted_data);
+  Serial.println(decrypted_data[4]);
+}
 
 void setup() {
   Serial.begin(115200);                                          //Start serial monitor
   Serial.println("ESP32 BLE Client - Shell Racing Car Remote");  //Intro message
 
+  mbedtls_aes_init(&aes);
+  mbedtls_aes_setkey_enc(&aes, aes_key, 128);
+  mbedtls_aes_setkey_dec(&aes, aes_key, 128);
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan();                                            //create new scan
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());  //Call the class that is defined above
   pBLEScan->setActiveScan(true);                                              //active scan uses more power, but get results faster
 
   // Make them with interl pullups
-  pinMode(PIN_UP, INPUT_PULLUP);
-  pinMode(PIN_DOWN, INPUT_PULLUP);
+  pinMode(PIN_FORWARD, INPUT_PULLUP);
+  pinMode(PIN_BACKWARDS, INPUT_PULLUP);
   pinMode(PIN_LEFT, INPUT_PULLUP);
   pinMode(PIN_RIGHT, INPUT_PULLUP);
+  pinMode(PIN_LIGHTS, INPUT_PULLUP);
+  button.attach(PIN_TURBO, INPUT_PULLUP);
+  button.interval(5);
+  button.setPressedState(LOW);
 }
 
 void loop() {
+  button.update();
   if (paired == false) {
     foundDevices = pBLEScan->start(3);  //Scan for 3 seconds to find the Shell Car
     while (foundDevices.getCount() >= 1) {
-      if (Server_BLE_Address != NULL &&paired == false) {
+      if (Server_BLE_Address != NULL && paired == false) {
         Serial.println("Found Device :-)... connecting to Server as client");
         if (connectToserver(*Server_BLE_Address)) {
           paired = true;
-          Serial.println("********************LED turned ON************************");
-          //digitalWrite (PIN_LED, LOW);
+          Serial.println("Paired successfully");
           break;
         } else {
           Serial.println("Pairing failed");
           break;
         }
       }
-
       if (paired == true) {
         Serial.println("Our device went out of range");
         paired = false;
-        Serial.println("********************LED OOOFFFFF************************");
-        //digitalWrite (PIN_LED, HIGH);
         ESP.restart();
         break;
       } else {
@@ -138,44 +179,10 @@ void loop() {
       }
     }
   } else {
-    //
     bool response = false;
-    if (!digitalRead(PIN_UP) && !digitalRead(PIN_LEFT)) {
-      Serial.println("DIR_UP_LEFT");
-      if (pRemoteCharacteristic != nullptr)
-        pRemoteCharacteristic->writeValue(up_left_data, 16, response);
-    } else if (!digitalRead(PIN_UP) && !digitalRead(PIN_RIGHT)) {
-      Serial.println("DIR_UP_RIGHT");
-      if (pRemoteCharacteristic != nullptr)
-        pRemoteCharacteristic->writeValue(up_right_data, 16, response);
-    } else if (!digitalRead(PIN_DOWN) && !digitalRead(PIN_LEFT)) {
-      Serial.println("DIR_DOWN_LEFT");
-      if (pRemoteCharacteristic != nullptr)
-        pRemoteCharacteristic->writeValue(down_left_data, 16, response);
-    } else if (!digitalRead(PIN_DOWN) && !digitalRead(PIN_RIGHT)) {
-      Serial.println("DIR_DOWN_RIGHT");
-      if (pRemoteCharacteristic != nullptr)
-        pRemoteCharacteristic->writeValue(down_right_data, 16, response);
-    } else if (!digitalRead(PIN_UP)) {
-      Serial.println("DIR_UP");
-      if (pRemoteCharacteristic != nullptr)
-        pRemoteCharacteristic->writeValue(up_data, 16, response);
-    } else if (!digitalRead(PIN_DOWN)) {
-      Serial.println("DIR_DOWN");
-      if (pRemoteCharacteristic != nullptr)
-        pRemoteCharacteristic->writeValue(down_data, 16, response);
-    } else if (!digitalRead(PIN_LEFT)) {
-      Serial.println("DIR_LEFT");
-      if (pRemoteCharacteristic != nullptr)
-        pRemoteCharacteristic->writeValue(left_data, 16, response);
-    } else if (!digitalRead(PIN_RIGHT)) {
-      Serial.println("DIR_RIGHT");
-      if (pRemoteCharacteristic != nullptr)
-        pRemoteCharacteristic->writeValue(right_data, 16, response);
-    } else {
-      Serial.println("idle");
-      if (pRemoteCharacteristic != nullptr)
-        pRemoteCharacteristic->writeValue(idle_data, 16, response);
+    craftBLErequest();
+    if (pRemoteCharacteristic != nullptr) {
+      pRemoteCharacteristic->writeValue(output_data, 16, response);
     }
     delay(100);
   }
